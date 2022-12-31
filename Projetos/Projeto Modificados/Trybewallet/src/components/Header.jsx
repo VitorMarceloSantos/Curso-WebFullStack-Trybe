763b640bc7fa36493fbc9224de2a2a54a266eaeb
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-12-31 10:57:29 -0300

    Header Css - TrybeWallet

--- Projetos/Projeto Modificados/Trybewallet/src/components/Header.jsx
+++ Projetos/Projeto Modificados/Trybewallet/src/components/Header.jsx
@@ -13,30 +13,33 @@ class Header extends Component {
           <FaMoneyBillWave className="icon-money" />
           <h1>DashBoard - TrybeWallet</h1>
         </div>
-        <section className="container-login">
-          <p data-testid="email-field">
-            Email:
-            {`${userState.email}`}
-          </p>
-        </section>
-        <section className="container-expenses">
-          <div className="container-expenses-total">
-            <p>Despesa Total:</p>
-            <p data-testid="total-field">
-              { (walletState.expenses).length === 0 ? (0).toFixed(2) : (
-                ((walletState.expenses)
-                  .reduce((acc, current) => acc + (current.value * (Number(
-                    current.exchangeRates[current.currency].ask,
-                  ))), 0)).toFixed(2)
-              )}
+        <div className="container-login-position">
+          <section className="container-login">
+            <p data-testid="email-field">
+              Email:
+              {`${userState.email}`}
             </p>
-          </div>
-          <div className="container-coin-header">
-            <p data-testid="header-currency-field">
-              BRL
-            </p>
-          </div>
-        </section>
+          </section>
+          <section className="container-expenses">
+            <div className="container-expenses-total">
+              <p>Despesa Total:</p>
+              <p data-testid="total-field">
+                { (walletState.expenses).length === 0 ? (0).toFixed(2) : (
+                  ((walletState.expenses)
+                    .reduce((acc, current) => acc + (current.value * (Number(
+                      current.exchangeRates[current.currency].ask,
+                    ))), 0)).toFixed(2)
+                )}
+              </p>
+            </div>
+            <div className="container-coin-header">
+              <p data-testid="header-currency-field">
+                BRL
+              </p>
+            </div>
+          </section>
+        </div>
+
       </div>
     );
   }
