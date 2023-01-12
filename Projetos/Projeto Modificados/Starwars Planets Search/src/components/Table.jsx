8c2f03dbd713aa0eeee8ca3c723d1f46758074c7
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-01-12 07:25:14 -0300

    Tables

--- Projetos/Projeto Modificados/Starwars Planets Search/src/components/Table.jsx
+++ Projetos/Projeto Modificados/Starwars Planets Search/src/components/Table.jsx
@@ -1,41 +1,60 @@
 import React, { useContext } from 'react';
 import informationAPI from '../context/DataContext';
+import '../styles/table.css';
 
 function Table() {
   const { planets, planetsFiltered } = useContext(informationAPI);
   return (
     <div>
-      {(planetsFiltered.length !== 0 ? planetsFiltered : planets).map(({
-        name,
-        rotation_period: rotationPeriod,
-        orbital_period: orbitalPeriod,
-        diameter,
-        climate,
-        gravity,
-        terrain,
-        surface_water: surfaceWater,
-        population,
-        films,
-        created,
-        edited,
-        url,
-      }) => (
-        <tr key={ name }>
-          <td data-testid="planet-name">{name}</td>
-          <td>{Number(rotationPeriod)}</td>
-          <td>{Number(orbitalPeriod)}</td>
-          <td>{Number(diameter)}</td>
-          <td>{climate}</td>
-          <td>{gravity}</td>
-          <td>{terrain}</td>
-          <td>{Number(surfaceWater)}</td>
-          <td data-testid="test-unknown">{population}</td>
-          <td>{films}</td>
-          <td>{created}</td>
-          <td>{edited}</td>
-          <td>{url}</td>
+      <table>
+        <tr>
+          <td>Name</td>
+          <td>Rotation</td>
+          <td>Orbital Period</td>
+          <td>Diameter</td>
+          <td>Climate</td>
+          <td>Gravity</td>
+          <td>Terrain</td>
+          <td>Surface</td>
+          <td>Population</td>
+          <td>Films</td>
+          <td>Created</td>
+          <td>Edited</td>
+          <td>Url</td>
         </tr>
-      ))}
+        {(planetsFiltered.length !== 0 ? planetsFiltered : planets).map(({
+          name,
+          rotation_period: rotationPeriod,
+          orbital_period: orbitalPeriod,
+          diameter,
+          climate,
+          gravity,
+          terrain,
+          surface_water: surfaceWater,
+          population,
+          films,
+          created,
+          edited,
+          url,
+        }) => (
+          <tr data-testid="planet-name" key={ name }>
+            <td>{name}</td>
+            <td>{Number(rotationPeriod)}</td>
+            <td>{Number(orbitalPeriod)}</td>
+            <td>{Number(diameter)}</td>
+            <td>{climate}</td>
+            <td>{gravity}</td>
+            <td>{terrain}</td>
+            <td>{Number(surfaceWater)}</td>
+            <td data-testid="test-unknown">{population}</td>
+            <td>{films}</td>
+            <td>{created}</td>
+            <td>{edited}</td>
+            <td>{url}</td>
+          </tr>
+
+        ))}
+      </table>
     </div>
   );
 }
