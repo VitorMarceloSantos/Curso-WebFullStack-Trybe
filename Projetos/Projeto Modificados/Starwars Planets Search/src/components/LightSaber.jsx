b28fb50cb0b62108194ee5f2f2c5eea74b1cd987
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-01-12 12:08:33 -0300

    Tables

--- Projetos/Projeto Modificados/Starwars Planets Search/src/components/LightSaber.jsx
+++ Projetos/Projeto Modificados/Starwars Planets Search/src/components/LightSaber.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/lightSaber.css';
 import saber from '../images/sabreluz.png';
 
 export default function LightSaber() {
+  let [count, setCount] = useState(Number(0));
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount(count += 1);
+    }, 4000);
+  }, count);
   return (
     <div className="container-light-saber">
       <div className="container-saber">
@@ -10,6 +17,7 @@ export default function LightSaber() {
           <img src={ saber } alt="Sabre de Luz" className="img-saber" />
         </div>
         <div className="laser" />
+        {console.log(count)}
       </div>
     </div>
   );
