797a44ab3df7382e10287f6926ee2907ce454d0f
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-01-15 16:21:50 -0300

    Exercicio 1

---
+++ Exercicios/Backend/Bloco 09/Aula 02/Exercicio-01.ts
@@ -0,0 +1,33 @@
+class Person{
+  private _name: string
+  private _birthDate: Date
+
+  constructor(
+    private n: string,
+    private b: Date
+  ){
+    this._name = n;
+    this._birthDate = b;
+  }
+
+  public get name(){
+    return this._name;
+  }
+  public get birthDate(){
+    return this._birthDate;
+  }
+  public set name(newValue: string){
+    if(newValue.length > 3) this._name = newValue;
+  }
+  public set birthDate(newValue: Date){
+    // Artigo: https://www.freecodecamp.org/portuguese/news/date-now-em-javascript-como-obter-a-data-atual-em-javascript/
+    // const actualDate = Date.now();
+    // const today = new Date(actualDate)
+    // console.log(today.toLocaleDateString()) // Mostrar a data atual no formato: dia/mes/ano
+  
+    const actualDate = new Date();
+    if((newValue <= actualDate) && ((2023 - Number(newValue.getFullYear)) < 120))
+     this._birthDate = newValue;
+    }
+
+}
